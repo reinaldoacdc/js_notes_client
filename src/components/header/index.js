a3e9dc877962205df1db61931cecbd06043332e9
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-01-28 17:09:22 -0300

    add routes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
-import { Navbar, Container } from 'rbx';
+import { Navbar, Container, Column } from 'rbx';
 import LogoImage from '../../assets/images/logo.png';
 import "../../styles/header.scss";
+import { Link } from 'react-router-dom';
 
 function Header(){
   return(
 <Navbar>
   <Container>
     <Navbar.Brand>
-      <img src={LogoImage} />
+      <Link to="/">
+        <img src={LogoImage} />
+      </Link>
       <Navbar.Burger
         className="navbar-burger burger" 
         aria-label="menu" 
@@ -21,12 +24,19 @@ function Header(){
     </Navbar.Brand>
 
     <Navbar.Menu id="navbar-menu">
-      <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
-        Item 1
-      </Navbar.Segment>
-    </Navbar.Menu>
-  </Container>
-</Navbar>
+          <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
+            <Column.Group>
+              <Column>
+                <Link to="/register" className="button is-white has-text-custom-purple">Register</Link>
+              </Column>
+              <Column>
+                <Link to="/login" className="button is-outlined is-custom-purple">Login</Link>
+              </Column>
+            </Column.Group>
+          </Navbar.Segment>
+        </Navbar.Menu>
+      </Container>
+    </Navbar>
   )
 }
 
---
+++ src/screens/auth/register/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Register = () => (
+  <Fragment>
+    Hello World
+  </Fragment>
+);
+
+export default Register;
