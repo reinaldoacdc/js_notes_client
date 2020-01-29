26d7ea0da522b51baa77d5ddcbb68f8109986061
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-01-29 12:58:12 -0300

    Add privateRoutes and header loggedIn"

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
+import HeaderLogged from "../../../components/header_logged";
 
 const Notes = () => (
   <Fragment>
+     <HeaderLogged/>
     Hello World
   </Fragment>
 );
