a78ee93ef0bb7f0fca41cdf81e9307483cf013fa
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-04-30 10:42:58 -0300

    Alterando maquina

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,35 +1,56 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Column, Button } from "rbx";
 import "../../styles/notes.scss";
+import List from "../notes/list";
+import NoteService from '../../services/note';
 import { push as Menu } from 'react-burger-menu'
 
 function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
 
   return (
     <Fragment>
-      <Column.Group className="notes" id="notes">
-        <Menu
-          pageWrapId={"notes-editor"}
-          isOpen={props.isOpen}
+      <div className="notes" id="notes">
+        <Menu 
+          pageWrapId={"notes-editor"} 
+          isOpen={ props.isOpen }
           onStateChange={(state) => props.setIsOpen(state.isOpen)}
-          disableAutoFocus
+          disableAutoFocus 
           outerContainerId={"notes"}
-          customBurgerIcon={false}
-          customCrossIcon={false}
+          customBurgerIcon={ false }
+          customCrossIcon={ false }
         >
-          <Column.Group>
-            <Column size={10} offset={1}>
-              Search...
-            </Column>
-          </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} />
         </Menu>
 
 
         <Column size={12} className="notes-editor" id="notes-editor">
           Editor...
         </Column>
-      </Column.Group>
+      </div>
     </Fragment>
   )
 }
